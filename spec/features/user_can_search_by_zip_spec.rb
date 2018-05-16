7c26b69e24a3f3a2b990c141ad76c0a0c378835f
Nikhil Shahi <shniks@gmail.com>
2018-05-16 11:03:28 -0600

    Final commit. Added additonal assertions for the test.

--- spec/features/user_can_search_by_zip_spec.rb
+++ spec/features/user_can_search_by_zip_spec.rb
@@ -13,12 +13,14 @@ describe 'User visits root path and fills in search form' do
         # Then I should be on page "/search"
         expect(current_path).to eq(search_path)
         # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-        expect(page).to have_css('.station', count: 2)
+        expect(page).to have_css('.station', count: 10)
         # And the stations should be limited to Electric and Propane
         # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
         within(first('.station')) do
           expect(page).to have_css('.name')
+          expect(page).to have_content('UDR 800')
           expect(page).to have_css('.address')
+          expect(page).to have_content('800 Acoma St, Denver, CO, 80204')
           expect(page).to have_css('.fuel_types')
           expect(page).to have_css('.distance')
           expect(page).to have_css('.access_times')
