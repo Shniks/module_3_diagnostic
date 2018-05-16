6c746482c90f854e19b5c936009298db0f135a7d
Nikhil Shahi <shniks@gmail.com>
2018-05-16 10:05:08 -0600

    Added faraday functionality to the station service.

--- app/services/station_service.rb
+++ app/services/station_service.rb
@@ -4,8 +4,21 @@ class StationService
   end
 
   def stations
-    conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.format?")
     binding.pry
+    raw_stations.map { |raw_station| Station.new(raw_station) }
+  end
 
+  def conn
+    conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.format?zip=#{@zipcode}&limit=10&radius=6.0&fuel_type=ELEC,LPG")
+  end
+
+  def response
+    response ||= conn.get do |request|
+      request.headers['X-API-KEY'] = ENV['API_KEY']
+    end
+  end
+
+  def raw_stations
+    JSON.parse(response.body, symbolize_names: true)[:results]
   end
 end
